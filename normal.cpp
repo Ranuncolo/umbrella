86c5ecaf4e51294c8b5aa97cf923f700836e942a
René Gaio <renegaio@Renes-MacBook-Pro.local>
2018-03-14 15:15:53 +0000

    lol

---
+++ normal.cpp
@@ -0,0 +1,8 @@
+//
+//  normal.cpp
+//  
+//
+//  Created by René Gaio on 14/03/2018.
+//
+
+#include <stdio.h>
---
+++ tempi2c.cpp
@@ -0,0 +1,8 @@
+//
+//  tempi2c.cpp
+//  
+//
+//  Created by René Gaio on 14/03/2018.
+//
+
+#include <stdio.h>
