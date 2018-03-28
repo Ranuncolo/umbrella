960aadd2dd4757a5045f34b638b2b6d2f6470106
René Gaio <35761300+Ranuncolo@users.noreply.github.com>
2018-03-28 19:57:32 +0100

    almost final version
    need to complete pressure and format output

---
+++ computation.cpp
@@ -0,0 +1,204 @@
+//
+//  computation.cpp
+//  
+//
+//  Created by René Gaio on 27/03/2018.
+//
+
+/* this .cpp file include the four different functions required to convert digital data into proper data, for each sensor.
+ */
+
+#include "computation.hpp"
+
+using namespace std;
+
+
+// ******************************************************************
+
+double calc_temp()
+{
+    unsigned char T_bit[2];
+    int Temp = 0;
+    double Temperature = NAN;
+    
+    //instance of class:
+    geti2c tempsensor(string("temperature"), temp_add, 2);
+    
+    //address register + check status, else change in NAN:
+    int flag = -1;
+    flag = tempsensor.add_register();
+    if (flag <0)
+    {
+        Temperature = NAN;
+        return Temperature;
+    }
+    //read register + check status, else change in NAN:
+    flag = tempsensor.read_register(*T_bit);
+    if (flag <0)
+    {
+        Temperature = NAN;
+        return Temperature;
+    }
+    // Convert into value:
+    std::bitset<16> Tcombo;
+    std::bitset<16>Tb1(T_bit[0]);
+    std::bitset<16>Tb2(T_bit[1]);
+    Tcombo = (Tb1 << 8) | Tb2;
+    
+    // last for 4 are just empty and they are not part of the number:
+    Tcombo = Tcombo >> 4;
+    // cast to int
+    
+    Temp = (int)(Tcombo.to_ulong());
+    // following the conversion on the data sheet, pag 5, set the difference between negative and positive temperatures, then convert to T [C].
+    // to convert, the data must be multiplied by 0.0625, the sensitivity (1/16)
+    if (Temp > 2047)
+    {
+        Temp = Temp - 4096;
+    }
+    Temperature = Temp * 0.0625;
+    
+    return Temperature;
+}
+
+
+// ******************************************************************
+
+double calc_hum()
+{
+    unsigned char H_bit[2];
+    int Hum = 0;
+    double Humidity = NAN;
+    
+    //instance of class:
+    geti2c humsensor(string("humidity"), hum_add, 2);
+    
+    //address register + check status, else change in NAN:
+    int flag = -1;
+    flag = humsensor.add_register();
+    if (flag <0)
+    {
+        Humidity = NAN;
+        return Humidity;
+    }
+    //read register + check status, else change in NAN:
+    flag = humsensor.read_register(*H_bit);
+    if (flag <0)
+    {
+        Humidity = NAN;
+        return Humidity;
+    }
+    // Convert into value:
+    std::bitset<16> Hcombo;
+    std::bitset<16>Hb1(H_bit[0]);
+    std::bitset<16>Hb2(H_bit[1]);
+    Hcombo = (Hb1 << 8) | Hb2;
+    
+    /* The first two bits are not part of the number, I must be sure they are zero. To do so, I first create a 14 bits binary number, called "bit14", full of 1 using the "set" function, then using the bitwise operation AND (&) I am sure I will have a 14 bit number.
+     
+     Further info at http://www.cplusplus.com/reference/bitset/bitset/set/
+     */
+    std::bitset<16> bit14;
+    bit14.set();
+    
+    /* At the moment its, a 16bits number full of 1, 0b1111111111111111. To set to zero the first two bits I use again the set function. This function starts to count from right to left, from zero, so the 14th and 15th entry are the two bits I want to be zero. */
+    
+    bit14.set(14,0);
+    bit14.set(15,0); //0b0011111111111111
+    
+    // Perform the & operation
+    Hcombo = (Hcombo & bit14);
+    
+    // In this way I am sure that it is a 14bit number, and I can convert into humidity through the equation written at page 6 of the data sheet.
+    
+    double denominator = 16382; // 2^14 - 2. Double for c++ reasons or it will not convert Hum into a double.
+    Hum = (int)(Hcombo.to_ulong()); // cast into intiger
+    Humidity = Hum / denominator * 100;
+    
+    return Humidity;
+}
+
+
+// ******************************************************************
+
+double calc_press()
+{
+    double Pressure = NAN;
+    return Pressure;
+}
+
+
+// ******************************************************************
+
+double calc_lux()
+{
+    unsigned char L_bit[2];
+    double Light = NAN;
+    
+    //instance of class:
+    geti2c lightsensor(string("light"), lux_add, 2);
+    
+    //address register + check status, else change in NAN:
+    int flag = -1;
+    flag = lightsensor.add_register();
+    if (flag <0)
+    {
+        Light = NAN;
+        return Light;
+    }
+    //read register + check status, else change in NAN:
+    flag = lightsensor.read_register(*L_bit);
+    if (flag <0)
+    {
+        Light = NAN;
+        return Light;
+    }
+    // Convert into value:
+    /* It has two component accordin to the data sheet, the first 4 bit are in the number Ebit, the latter 12 in the number Rbit.
+     */
+    // Definition of the two components:
+    std::bitset<8>Ebit;
+    std::bitset<16>Rbit;
+    
+    // Ebit:
+    // set the first bit in a byte callet Lbit4, and the bit4 (11110000):
+    std::bitset<8> Lbit4(L_bit[0]);
+    std::bitset<8> bit4(string("11110000"));
+    
+    // AND operation e shift operator (I want 0000XXXX):
+    Ebit = Lbit4 & bit4;
+    Ebit = (Ebit >> 4);
+    
+    // Rbit:
+    // This part is similar to the others, required for Rbit.
+    std::bitset<16> Lcombo;
+    std::bitset<16>Lb1(L_bit[0]);
+    std::bitset<16>Lb2(L_bit[1]);
+    Lcombo = (Lb1 << 8) | Lb2;
+    
+    std::bitset<16> bit12(string("0000111111111111"));
+    Rbit = Lcombo & bit12;
+    
+    // Now I combine them according to the data sheet, pag 20:
+    int E = (int)(Ebit.to_ulong());
+    int R = (int)(Rbit.to_ulong());
+    Light = 0.01 * pow(2.0,E) * R;
+    
+    return Light;
+}
+
+
+// ******************************************************************
+
+void epoch(char &tempo)
+{
+    time_t data_epoch;
+    struct tm *tepoch;
+    char *epoch_time;
+    epoch_time = &tempo;
+    time(&data_epoch);
+    tepoch = localtime(&data_epoch);
+    strftime(epoch_time,17,"%F %H:%M",tepoch);
+}
+
+
--- umbrella.cpp
+++ umbrella.cpp
@@ -1,6 +1,6 @@
 //
 //  umbrella.cpp
-//  
+//
 //
 
 /* C++ code for umbrella - weather station on RPi.
@@ -25,9 +25,9 @@
  where "<your_name>" can be anything you wish.
  
  ex:
- $ g++ umbrella.cpp -o weather
+ $ g++ umbrella.cpp geti2c.cpp computation.cpp -o weather
  $ ./weather
-
+ 
  */
 // ************************************************************************
 // Libraries:
@@ -40,8 +40,8 @@
 
 // for i2c +++ check if .h is required
 #include <linux/types.h>
-#include <linux/i2c-dev>
-#include <ioctl.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
 #include <unistd.h> // for read()
 #include <fcntl.h> // Required for O_RDWR
 #include <bitset> // required for bit operation
@@ -56,6 +56,7 @@
 //#include <cstring>
 //#include <string>
 
+#include "computation.hpp"
 using namespace std;
 
 
@@ -74,41 +75,21 @@ static const char *device = "/dev/i2c-1"; //for our RPi, other may have 0
  */
 
 static int temp_add = 0x48;
-static int hum_add = 0x21;
+static int hum_add = 0x27;
 static int pres_add = 0xC0;
 static int lux_add = 0x44;
 
-//+++ Check, only the temps one is correct.
+//+++ Check, not all are correct.
 
 int main()
 {
-    
+    // Definition of values:
+    double Temperature = NAN;
+    double Humidity = NAN;
+    double Pressure = NAN;
+    double Light = NAN;
     // ********************************************************************
-    // Definition of variables required for I2C data retrieving:
-    
-    int fd, Tfd, Hfd, Pfd, Lfd;
-    /* Those intigers are required for the ioctl function, they are called "file descriptors." Tfd, Hfd, Pfd and Lfd are respectively the file descriptor for the temperature, humidity, pressure and light sensor. fd is the general first descriptor for I2C bus in general.
-     Further info at: http://man7.org/linux/man-pages/man2/ioctl.2.html
-     */
-    
-    unsigned char T_bit[2];
-    unsigned char H_bit[2];
-    unsigned char P_bit[2];
-    unsigned char L_bit[2];
-    /* Those are arrays of char which will store the digital binary data for each sensor respectively.
-     +++ check their length. Maybe not all of them have this length.
-     */
-    
-    double Temperature;
-    double Humidity;
-    double Pressure;
-    double Light;
-    //+++ maybe it would be better to initialise them, I would like NaN.
-    
-    
-    
-    // ********************************************************************
-    // Creation of the file:
+    // Creation of the output file:
     // File name is the current local time. It requires <ctime>.
     // More info at http://www.cplusplus.com/reference/ctime/strftime
     
@@ -140,204 +121,23 @@ int main()
     file << "------------------------------------------" << endl;
     file << endl;
     
-    // ********************************************************************
-    // Searching for sensor:
-    cout << "Searching for sensors:" << endl;
-    
-    fd = open(device, O_RDWR);
-    /* open the I2C pathname given. It returns a positive descriptor if succeeded.
-     More info at: http://man7.org/linux/man-pages/man2/open.2.html
-     */
-    
-    if (fd < 0)
-    {
-        cout << "Could not open I2C device!" << endl;
-        return 1;
-    }
-    
-    
-    // I set all the other descriptors equal to fd. This would not be necessary, but I can keep each sensor with its own prefix.
-    //+++ check, probably fd is the number of sensors read. Not sure.
-    Tfd = fd;
-    Hfd = fd;
-    Pfd = fd;
-    Lfd = fd;
-    
-    //+++ should be nice to print how many and which sensors we have.
-    
-    
-    
-    // ********************************************************************
-    // Set the slave address for each sensor + status check:
-    
-    if (ioctl(Tfd, I2C_SLAVE, temp_add) < 0 )
-    {
-        cout << "Can't reach the temperature sensor!" << endl;
-        Temperature = NAN;
-    }
-    
-    if (ioctl(Hfd, I2C_SLAVE, hum_add) < 0 )
-    {
-        cout << "Can't reach the humidity sensor!" << endl;
-        Humidity = NAN;
-    }
-    
-    if (ioctl(Pfd, I2C_SLAVE, pres_add) < 0 )
-    {
-        cout << "Can't reach the pressure sensor!" << endl;
-        Pressure = NAN;
-    }
-    
-    if (ioctl(Lfd, I2C_SLAVE, lux_add) < 0 )
-    {
-        cout << "Can't reach the light sensor!" << endl;
-        Light = NAN;
-    }
-    
-    
-    // ********************************************************************
+    //  ********************************************************************
     // Header of the output file:
     
     cout << "Epoch\t Temp [C]\t Hum [%]\t Pres [Pa]\t Light [lux]" << endl;
     file << "Epoch\t Temp [C]\t Hum [%]\t Pres [Pa]\t Light [lux]" << endl;
     
-    //+++ CHECK PRESSURE AND LIGHT UNITS!!!!
-    
-    
-    
     // Start the while loop:
     while(1) //+++ I LL HAVE TO CHOOSE A PROPER WHILE, WHILE THE DAY
     {
+        char Epoch[18] = {};
+        epoch(*Epoch);
         
-        // ****************************************************************
-        // Read from the sensors + status check:
-        /* The read function reads for the number of bytes written in its third argument and store it in the buffer (unsigned char) defined at the beginning. It returns the number of bytes read, so it is possible to check it.
-         Further info at: http://man7.org/linux/man-pages/man2/read.2.html
-         */
+        Temperature = calc_temp();
+        Humidity = calc_hum();
+        Pressure = calc_press();
+        Light = calc_lux();
         
-        
-        if (read(Tfd,T_bit,2) != 2)
-        {
-            cout << "Failed to read temperature data." << endl;
-            Temperature = NAN;
-        }
-        
-        if (read(Hfd,H_bit,2) != 2)
-        {
-            cout << "Failed to read humidity data." << endl;
-            Humidity = NAN;
-        }
-        
-        if (read(Pfd,P_bit,2) != 2)
-        {
-            cout << "Failed to read pressure data." << endl;
-            Pressure = NAN;
-        }
-        
-        if (read(Lfd,L_bit,2) != 2)
-        {
-            cout << "Failed to read light data." << endl;
-            Light  = NAN;
-        }
-        
-        // TEMPERATURE:
-        // concatenate the two byte inTO a 16 bit binary number:
-        std::bitset<16> Tcombo;
-        std::bitset<16>Tb1(T_bit[0]);
-        std::bitset<16>Tb2(T_bit[1]);
-        Tcombo = (Tb1 << 8) | Tb2;
-        
-        // last for 4 are just empty and they are not part of the number:
-        Tcombo = Tcombo >> 4;
-        // cast to int
-        int Temp;
-        Temp = (int)(combo.to_ulong());
-        // following the conversion on the data sheet, pag 5, set the difference between negative and positive temperatures, then convert to T [C].
-        // to convert, the data must be multiplied by 0.0625, the sensitivity (1/16)
-        if (Temp > 2047)
-        {
-            Temp = Temp - 4096;
-        }
-        Temperature = Temp * 0.0625;
-    
-        
-        // HUMIDITY:
-        std::bitset<16> Hcombo;
-        std::bitset<16>Hb1(H_bit[0]);
-        std::bitset<16>Hb2(H_bit[1]);
-        Hcombo = (Hb1 << 8) | Hb2;
-        
-        /* The first two bits are not part of the number, I must be sure they are zero. To do so, I first create a 14 bits binary number, called "bit14", full of 1 using the "set" function, then using the bitwise operation AND (&) I am sure I will have a 14 bit number.
-         
-         Further info at http://www.cplusplus.com/reference/bitset/bitset/set/
-         */
-        std::bitset<16> bit14;
-        bit14.set();
-        
-        /* At the moment its, a 16bits number full of 1, 0b1111111111111111. To set to zero the first two bits I use again the set function. This function starts to count from right to left, from zero, so the 14th and 15th entry are the two bits I want to be zero. */
-        
-        bit14.set(14,0);
-        bit14.set(15,0); //0b0011111111111111
-        
-        // Perform the & operation
-        Hcombo = (Hcombo & bit14);
-    
-        // In this way I am sure that it is a 14bit number, and I can convert into humidity through the equation written at page 6 of the data sheet.
-        
-        double denominator = 16382; // 2^14 - 2. Double for c++ reasons or it will not convert Hum into a double.
-        int Hum = (int)(Hcombo.to_ulong()); // cast into intiger
-        Humidity = Hum / denominator * 100;
-        
-        
-        // PRESSURE:
-        //+++ on going
-        
-        
-        // LIGHT:
-        /* It has two component accordin to the data sheet, the first 4 bit are in the number Ebit, the latter 12 in the number Rbit.
-         */
-        
-        // Definition of the two components:
-        std::bitset<8>Ebit;
-        std::bitset<16>Rbit;
-        
-        // Ebit:
-        // set the first bit in a byte callet Lbit4, and the bit4 (11110000):
-        std::bitset<8> Lbit4(L_bit[0]);
-        std::bitset<8> bit4(string("11110000"));
-        
-        // AND operation e shift operator (I want 0000XXXX):
-        Ebit = Lbit4 & bit4;
-        Ebit = (Ebit >> 4);
-        
-        // Rbit:
-        // This part is similar to the others, required for Rbit.
-        std::bitset<16> Lcombo;
-        std::bitset<16>Lb1(L_bit[0]);
-        std::bitset<16>Lb2(L_bit[1]);
-        Lcombo = (Lb1 << 8) | Lb2;
-        
-        std::bitset<16> bit12(string("0000111111111111"));
-        Rbit = Lcombo & bit12;
-        
-        // Now I combine them according to the data sheet, pag 20:
-        int E = (int)(Ebit.to_ulong());
-        int R = (int)(Rbit.to_ulong());
-        Light = 0.01 * pow(2.0,E) * R;
-        
-        
-        // ****************************************************************
-        // Epoch, the time of the actual data read:
-        
-        time_t data_epoch;      //name of the chosen object, epoch.
-        struct tm *tepoch;
-        char Epoch[10];
-        time(&data_epoch);
-        tepoch = localtime(&data_epoch); //
-        strftime(Epoch,10,"%F %H:%M",tepoch);
-        //+++ remember to change as Dan told you
-        
-        // ****************************************************************
         // Writing in the output file:
         
         cout << Epoch   << "\t" <<  Temperature
@@ -356,25 +156,14 @@ int main()
         // ****************************************************************
         // Wait 15 minutes
         int min = 15; // minutes you want to wait --> sample time
-        int msec = 15 * 60 * 1000 * 1000;
+        int msec = min * 60 * 1000 * 1000;
+        msec = 1000000; //+++
         usleep(msec); //usleep is in microsecond, so 900 x 10^6 microsec
         
         
         
-        
-        
-        
-        close(fd);
-    } //end of while - sampling.
+    }
     
-    
-    
-    
-    
-    
-    
-    // close the output file  +++ MAYBE IF END OF THE DAY
     file.close();
     return 0;
-    
 }
